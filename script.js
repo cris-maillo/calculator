857358973ff6a498699b4cb82ee0f53215245c70
cris-maillo <cristinamaillo.r@gmail.com>
2022-02-25 17:03:14 +0000

    added functionality

--- script.js
+++ script.js
@@ -1,39 +1,96 @@
-let result;
-let operator;
-let screen = document.getElementsByClassName("screen");
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
 
-function add(num1, num2) {
-    result = num1 + num2;
-    return result;
-}
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
 
-function subtract(num1, num2) {
-    result = num1 - num2;
-    return result;
-}
-
-function multiply(num1, num2) {
-    result = num1 * num2;
-    return result;
-}
-
-function divide(num1, num2) {
-    result = num1 / num2;
-    return result;
-}
-
-function operate(operator, num1, num2) {
-    if (operator == "add") {
-        add(num1, num2);
-    }else if(operator == "subtract"){
-        subtract(num1, num2);
-    }else if(operator == "multiply"){
-        multiply(num1, num2);
-    }else{
-        divide(num1, num2);
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
     }
-    
-    return result;
+
+    console.log(calculator);
 }
 
-console.log(operate("divide", 5, 6));
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && calculator.waitingForSecondOperand)  {
+        calculator.operator = nextOperator;
+        console.log(calculator);
+        return;
+      }
+  
+    if (firstOperand === null && !isNaN(inputValue)) {
+      calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const result = calculate(firstOperand, inputValue, operator);
+    
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+  
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+    console.log(calculator);
+  }
+
+function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+        return firstOperand + secondOperand;
+    } else if (operator === '-') {
+        return firstOperand - secondOperand;
+    } else if (operator === '*') {
+        return firstOperand * secondOperand;
+    } else if (operator === '/') {
+        return firstOperand / secondOperand;
+    }
+
+    return secondOperand;
+}
+
+function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+    console.log(calculator);
+  }
+
+function updateDisplay() {
+    const display = document.querySelector('.calculatorscreen');
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
+  
+const keys = document.querySelector('.calculatorkeys');
+keys.addEventListener('click', (event) => {
+
+  const { target } = event;
+
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    handleOperator(target.value);
+    updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    resetCalculator();
+    updateDisplay();
+    return;
+  }
+    inputDigit(target.id);
+    updateDisplay();
+});
